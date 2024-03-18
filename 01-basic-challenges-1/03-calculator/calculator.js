c89d86c91866fd7cdce57af599cc1cb1dc8159c5
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-03-18 09:58:17 +0200

    resolve calculator challenge

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,30 @@
-function calculator() {}
+// Define a functinat called calculator that takes 2 numbers and 1 operator sign
+// Goal: Return the result of the calculation
+// Conditions: - The function must return a number
+//             - The function must return a console log if an invalid operator is given
+
+const calculator = (a, b, operator) => {
+  switch (operator) {
+    case '+':
+      return a + b;
+      break;
+
+    case '-':
+      return a - b;
+      break;
+
+    case '*':
+      return a * b;
+      break;
+
+    case '/':
+      return a / b;
+      break;
+
+    default:
+      return console.log(`Invalid operator ${operator}`);
+      break;
+  }
+};
 
 module.exports = calculator;
