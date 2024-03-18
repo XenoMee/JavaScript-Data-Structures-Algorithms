db8d8dd54a6f17142b180e71674b4ff12b617da4
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-03-18 14:27:21 +0200

    finish reverse-string challenge

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,17 @@
-function reverseString() {}
+// Define: Function called reverseString that takes a string
+// Goal: Retrun the reverse of that string
+// Conditions: The input string will only containt lowercase letters and spaces
+
+// Plan:
+// 1. Itterate through the string characters backwards
+// 2.
+
+function reverseString(str) {
+  let reverseString = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverseString += str[i];
+  }
+  return reverseString;
+}
 
 module.exports = reverseString;
