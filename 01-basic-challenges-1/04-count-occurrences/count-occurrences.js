44c3ef43a2e0902b6d66cd789e68bafed67a8970
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-03-18 11:09:03 +0200

    resolve count occurences challenge

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,14 @@
-function countOccurrences() {}
+// Define: Function called countOccurences that takes a string and a character
+// Goal: Return the number of occurrences of that character in the string
+// Condition: Lowercase and uppercase characters are considered different characters.
+
+// 1. Define a count variable that increments everytime the given character is encountered
+// 2. Itterate through the string characters
+// 3. Compare each character with the given character
+// 4. Increment the count variable
+// 5. Return the count
+
+const countOccurrences = (string, character) =>
+  string.split(character).length - 1;
 
 module.exports = countOccurrences;
