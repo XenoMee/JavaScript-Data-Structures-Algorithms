a57698446f52d086aace55c6c3b4ca3603dcd06c
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-03-18 11:24:57 +0200

    resolve find-max-number challenge

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,17 @@
-function findMaxNumber() {}
+// Define: Function called findMaxNumber that takes an array of numbers as a parameter
+// Goal: Return the largest number in that array
+// Condition: The function must return a number
 
+// Plan:
+//
+// 1. Create a maxNumber variable
+// 2. Itterate through the array
+
+const findMaxNumber = (arr) => {
+  let maxNumber = 0;
+  arr.forEach((number) => {
+    if (number > maxNumber) maxNumber = number;
+  });
+  return maxNumber;
+};
 module.exports = findMaxNumber;
