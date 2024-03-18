ddd785eabae1662b8518f3762e9850e97d13ff75
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-03-18 16:57:49 +0200

    finish remove-duplicates challenge

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,21 @@
-function removeDuplicates() {}
+// Goal: Return a new array with duplicates removed
+// Conditions: The array can contain any data type
+
+// Plan:
+// 1. Create an empty array
+// 2. Itterate through the passed array elements
+// 3. Check if the elements is in the empty array
+// 4. Add the elements if it doesn't exist
+// 5. Return the new array
+
+function removeDuplicates(arr) {
+  const noDuplicatesArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (!noDuplicatesArray.includes(arr[i])) {
+      noDuplicatesArray.push(arr[i]);
+    }
+  }
+  return noDuplicatesArray;
+}
 
 module.exports = removeDuplicates;
