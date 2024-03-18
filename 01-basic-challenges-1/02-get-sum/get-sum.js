0d5d963f208029f9f084a2689a1d64068a03b968
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-03-18 09:37:07 +0200

    finish getSum challenge

--- 01-basic-challenges-1/02-get-sum/get-sum.js
+++ 01-basic-challenges-1/02-get-sum/get-sum.js
@@ -1,3 +1,7 @@
-function getSum() {}
+// Define: Function called getSum() that takes two parameters
+// Goal: Return the sum of two numbers
+// Condition: The function must return a number
+
+const getSum = (a, b) => a + b;
 
 module.exports = getSum;
