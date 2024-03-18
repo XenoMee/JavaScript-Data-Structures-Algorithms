3ff9e791e50dd3c433f2e286b5e2285d38eaac7e
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-03-18 16:32:58 +0200

    resolve count-vowels challenge

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,32 @@
-function countVowels() {}
+// Goal: Return the number of vowels in the string
+
+function countVowels(str) {
+  const formattedString = str.toLowerCase();
+  let count = 0;
+  for (let i = 0; i < formattedString.length; i++) {
+    const char = formattedString[i];
+    switch (char) {
+      case 'a':
+        count += 1;
+        break;
+      case 'e':
+        count += 1;
+        break;
+      case 'i':
+        count += 1;
+        break;
+      case 'o':
+        count += 1;
+        break;
+      case 'u':
+        count += 1;
+        break;
+
+      default:
+        console.log(char);
+    }
+  }
+  return count;
+}
 
 module.exports = countVowels;
