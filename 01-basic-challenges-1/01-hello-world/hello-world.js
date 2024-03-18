d1fbc619e42a5390d70c2602668d97c4abee9a1c
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-03-18 09:19:12 +0200

    finish hello world challenge

--- 01-basic-challenges-1/01-hello-world/hello-world.js
+++ 01-basic-challenges-1/01-hello-world/hello-world.js
@@ -1,3 +1,6 @@
-function helloWorld() {}
+// Goal: Create a function that returns "Hello World;
+// Condition: It must return a string
+
+const helloWorld = () => 'Hello World!';
 
 module.exports = helloWorld;
