13fc97b175837ac362d5cff762cc72a7bd7db0c8
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-03-19 10:30:08 +0200

    resolve array-intersection challenge

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,12 @@
-function arrayIntersection() {}
+// Define: 2 arrays as parameters
+// Goal: Return an array containing the intersection of the two arrays
+
+function arrayIntersection(arr1, arr2) {
+  const commonArray = [];
+  arr1.forEach((el) => {
+    if (arr2.includes(el)) commonArray.push(el);
+  });
+  return commonArray;
+}
 
 module.exports = arrayIntersection;
