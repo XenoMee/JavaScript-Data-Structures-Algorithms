5f32ff85c2f53df95bf6cd9592d929662b450a79
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-03-19 16:52:45 +0200

    resolve find-missing-letter

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,14 @@
-function findMissingLetter() {}
+// Define: a function called findMissingLetter that takes as parameter an array of consecutive letters
+// Goal: Return the missing letter
+
+const findMissingLetter = (letters) => {
+  let start = letters[0].charCodeAt(0);
+  for (let i = 1; i < letters.length; i++) {
+    const current = letters[i].charCodeAt(0);
+    if (current - start > 1) return String.fromCharCode(start + 1);
+    start = current;
+  }
+  return '';
+};
 
 module.exports = findMissingLetter;
