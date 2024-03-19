f55bdc43bcd392978b71020e755a84e69386a7bc
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-03-19 10:18:51 +0200

    resolve fizzbuzz challenge

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,18 @@
-function fizzBuzzArray() {}
+// Goal: Return an array of a given number length
+
+// Plan:
+// 1. Create an empty array variable
+// 2.
+
+function fizzBuzzArray(n) {
+  const numbersArray = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 15 === 0) numbersArray.push('FizzBuzz');
+    else if (i % 3 === 0) numbersArray.push('Fizz');
+    else if (i % 5 === 0) numbersArray.push('Buzz');
+    else numbersArray.push(i);
+  }
+  return numbersArray;
+}
 
 module.exports = fizzBuzzArray;
