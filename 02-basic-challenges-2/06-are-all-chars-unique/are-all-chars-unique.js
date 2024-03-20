ab1d82b503d8d60e784116e74a38d9ee7e22eddc
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-03-20 08:30:07 +0200

    resolve are-all-chars-unique challenge

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,17 @@
-function areAllCharactersUnique() {}
+// Define: a string as a parameter
+// Goal: Return true or false depending whether all
+// characters in the string are unique
+
+function areAllCharactersUnique(str) {
+  const uniqueStr = new Set();
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (uniqueStr.has(char)) return false;
+
+    uniqueStr.add(char);
+  }
+  return true;
+}
 
 module.exports = areAllCharactersUnique;
