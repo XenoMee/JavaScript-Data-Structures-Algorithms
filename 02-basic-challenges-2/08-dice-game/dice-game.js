06928b820851339c199e306bd2b0e128e3f2839f
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-03-20 10:50:51 +0200

    resolve dice-game challenge

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,26 @@
-function diceGameSimulation() {}
+// Goal: Return an array of objects, each object representing
+// a simulation result
+
+// Create 2 random numbers between 1 and 6
+// Create the object with the mentioned properties
+
+function diceGameSimulation(num) {
+  const simulationsArr = [];
+
+  for (let i = 0; i < num; i++) {
+    const dice1 = Math.floor(Math.random() * 6) + 1;
+    const dice2 = Math.floor(Math.random() * 6) + 1;
+    const sum = dice1 + dice2;
+    let result = '';
+
+    if (sum === 7 || sum === 11) result = 'win';
+    else if (sum === 12 || sum === 2 || sum === 3) result = 'lose';
+    else result = 'roll again';
+
+    simulationsArr.push({ dice1, dice2, sum, result });
+  }
+
+  return simulationsArr;
+}
 
 module.exports = diceGameSimulation;
