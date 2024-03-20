52d63ce57b64a4b096492dfe65f3e633b28777ff
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-03-20 10:24:44 +0200

    resolve first-non-repeating challenge

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,18 @@
-function findFirstNonRepeatingCharacter() {}
+// Define: take a string as a parameter
+// Goal: Return the first non-repeating character in the string
+
+function findFirstNonRepeatingCharacter(str) {
+  const charCount = {};
+
+  for (const char of str) {
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+
+  for (const char of str) {
+    if (charCount[char] === 1) return char;
+  }
+
+  return null;
+}
 
 module.exports = findFirstNonRepeatingCharacter;
